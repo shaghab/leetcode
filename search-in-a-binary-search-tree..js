6c8e07242e19d6b251802aff4166ad334ef1a673
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-04-14 21:50:31 +0500

    solved without recursion as well

--- search-in-a-binary-search-tree..js
+++ search-in-a-binary-search-tree..js
@@ -18,11 +18,34 @@ const searchBST = (root, val) => {
   if (root.val === val) return root;
 
   let ans = null;
-  if (root.left !== null) ans = searchBST(root.left, val);
+  //if (root.left !== null)
+  ans = searchBST(root.left, val);
   if (ans !== null) return ans;
 
-  if (root.right !== null) ans = searchBST(root.right, val);
+  //if (root.right !== null)
+  ans = searchBST(root.right, val);
   if (ans !== null) return ans;
+
+  return null;
+};
+
+const searchBST_loop = (root, val) => {
+  if (root === null) return null;
+  if (root.val === val) return root;
+
+  let ans = null;
+
+  let nodes = [];
+  if (root.left !== null) nodes.push(root.left);
+  if (root.right !== null) nodes.push(root.right);
+
+  while (nodes.length > 0) {
+    let node = nodes.pop();
+    if (node.val === val) return node;
+
+    if (node.left !== null) nodes.push(node.left);
+    if (node.right !== null) nodes.push(node.right);
+  }
 
   return null;
 };
